6262eb31f7c6aa187d4bf80213fcc3feb5120c9b
Paul Jung <pbnjcub@gmail.com>
2023-02-09 19:13:21 -0500

    commit tree 9034394bab1da6e56c9d1c035809e21820b33040

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,17 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-    [1,2,3,4]
+  ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = array << element
-  new_array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  new_array = array.unshift(element)
-  new_array
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -29,9 +27,72 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.first
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.last
+  array[-1]
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def create_an_empty_array
+#   []
+# end
+
+# def create_an_array
+#     [1,2,3,4]
+# end
+
+# def add_element_to_end_of_array(array, element)
+#   new_array = array << element
+#   new_array
+# end
+
+# def add_element_to_start_of_array(array, element)
+#   new_array = array.unshift(element)
+#   new_array
+# end
+
+# def remove_element_from_end_of_array(array)
+#   array.pop
+# end
+
+# def remove_element_from_start_of_array(array)
+#   array.shift
+# end
+
+# def retrieve_element_from_index(array, index_number)
+#   array[index_number]
+# end
+
+# def retrieve_first_element_from_array(array)
+#   array.first
+# end
+
+# def retrieve_last_element_from_array(array)
+#   array.last
+# end
